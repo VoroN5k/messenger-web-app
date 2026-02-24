b58b83aa27a5bfd5f725f27dc0bd09ebbd692af6
VoroN5k <Volovykartem@gmail.com>
2026-02-24 17:30:19 +0200

    initialize Nest.js application with Prisma setup and basic configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+    dotenv.config();
+
+    const app = await NestFactory.create(AppModule);
+
+
+    app.setGlobalPrefix('api');
+
+
+    app.enableCors({
+        origin: ['http://localhost:3000'],
+        credentials: true,
+    });
+
+    await app.listen(process.env.PORT || 4000);
+    console.log(`Server running on http://localhost:${process.env.PORT || 4000}`);
 }
+
 bootstrap();
