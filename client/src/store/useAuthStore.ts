b0d427972f17e5c70abaf25189332ffce56c6455
VoroN5k <Volovykartem@gmail.com>
2026-03-03 18:53:59 +0200

    feat: implement authentication store with Zustand

---
+++ client/src/store/useAuthStore.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface AuthState {
+    user: any | null;
+    accessToken: string | null;
+    setAuth: (user: any, token: string) => void;
+    logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>()(
+    persist(
+        (set) => ({
+            user: null,
+            accessToken: null,
+            setAuth: (user, token) => set({ user, accessToken: token}),
+            logout: () => set({ user: null, accessToken: null }),
+        }),
+        { name: 'auth-storage' } // Save token in storage automatically
+    )
+)
