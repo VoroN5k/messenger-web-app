9982f0eeede701d8b651e055134a3fe93bf63af5
VoroN5k <Volovykartem@gmail.com>
2026-04-27 15:44:02 +0300

    feat: update dependencies and integrate UploadModule into ReportsModule

--- server/src/reports/reports.module.ts
+++ server/src/reports/reports.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller.js';
 import { ReportsService } from './reports.service.js';
 import { EmailModule } from '../auth/email/email.module.js';
+import { UploadModule } from '../upload/upload.module.js';
 
 @Module({
-  imports: [EmailModule],
+  imports: [EmailModule, UploadModule],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
