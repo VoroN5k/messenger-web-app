fb6cbd3813b8bbdf15ccaf5883ed4e8b15f10c47
VoroN5k <volovykartem@gmail.com>
2026-04-30 23:24:16 +0300

    feat: add TypeScript definitions for jsqr module

---
+++ client/src/types/jsqr.d.ts
@@ -0,0 +1,9 @@
+declare module 'jsqr' {
+    interface QRCode { data: string }
+    function jsQR(
+        data: Uint8ClampedArray,
+        width: number,
+        height: number,
+    ): QRCode | null;
+    export = jsQR;
+}
